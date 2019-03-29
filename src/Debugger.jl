7026f5d5019d0f6a0576a71e47c2fae76eafb035
Kristoffer Carlsson <kcarlsson89@gmail.com>
2019-03-29 17:19:43 +0100

    add an error message if trying to enter a compiled function

--- src/Debugger.jl
+++ src/Debugger.jl
@@ -86,6 +86,7 @@ function _make_frame(mod, arg)
     quote
         theargs = $(esc(args))
         frame = JuliaInterpreter.enter_call_expr(Expr(:call,theargs...))
+        frame === nothing && error("failed to enter the function, perhaps it is set to run in compiled mode")
         frame = JuliaInterpreter.maybe_step_through_kwprep!(frame)
         frame = JuliaInterpreter.maybe_step_through_wrapper!(frame)
         JuliaInterpreter.maybe_next_call!(frame)
