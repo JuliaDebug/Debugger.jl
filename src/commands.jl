10152fad58d31256d29882e9434e20ee1fd7337e
Elliot Saba <staticfloat@gmail.com>
2019-07-20 19:22:51 -0400

    Switch meaning of `up`/`down` to better match other debuggers (#195)

--- src/commands.jl
+++ src/commands.jl
@@ -131,12 +131,12 @@ end
 function execute_command(state::DebuggerState, v::Union{Val{:up}, Val{:down}}, cmd::AbstractString)
     args = split(cmd, " ")[2:end]
     if isempty(args)
-        offset = v == Val(:up) ? -1 : +1
+        offset = v == Val(:up) ? +1 : -1
     else
         length(args) > 1 && return invalid_command(state, cmd)
         offset = tryparse(Int, args[1])
         offset == nothing && return invalid_command(state, cmd)
-        v == Val(:up) && (offset *= -1)
+        v == Val(:down) && (offset *= -1)
     end
     return execute_command(state, Val(:f), string("f ", state.level + offset))
 end
